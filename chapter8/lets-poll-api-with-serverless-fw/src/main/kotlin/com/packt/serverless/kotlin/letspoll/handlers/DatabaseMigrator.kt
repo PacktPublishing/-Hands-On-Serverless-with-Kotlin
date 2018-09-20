b4ff6f5a0522be0d2b9c8d7dac8fba97921c8da4
ameyawebonise <ameya.kulkarni@weboniselab.com>
2018-09-20 19:41:39 +0530

    Chagnes

---
+++ chapter8/lets-poll-api-with-serverless-fw/src/main/kotlin/com/packt/serverless/kotlin/letspoll/handlers/DatabaseMigrator.kt
@@ -0,0 +1,40 @@
+package com.packt.serverless.kotlin.letspoll.handlers
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.packt.serverless.kotlin.letspoll.ApiGatewayResponse
+import liquibase.LabelExpression
+import liquibase.Contexts
+import liquibase.resource.ClassLoaderResourceAccessor
+import liquibase.Liquibase
+import liquibase.database.jvm.JdbcConnection
+import liquibase.database.DatabaseFactory
+import com.packt.serverless.kotlin.letspoll.commons.DatabaseAccessUtils
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import org.apache.logging.log4j.LogManager
+
+
+class DatabaseMigrator : RequestHandler<Map<String, Any>, ApiGatewayResponse> {
+    internal var conn = DatabaseAccessUtils.simpleConnection
+    override  fun handleRequest(input: Map<String, Any>, context: Context): ApiGatewayResponse {
+
+        try {
+            val dataBase = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(
+                    JdbcConnection(conn))
+            val liquiBase = liquibase.Liquibase("letsPoll.changelog.xml", ClassLoaderResourceAccessor(), dataBase)
+            liquiBase.update(Contexts(), LabelExpression())
+        } catch (e: Exception) {
+            LOG.error("Exception occured in migrating the database")
+            e.printStackTrace()
+        }
+
+        return ApiGatewayResponse.build {
+            statusCode = 200
+        }
+
+    }
+
+    companion object {
+        private val LOG = LogManager.getLogger(DatabaseMigrator::class.java)
+    }
+
+}
