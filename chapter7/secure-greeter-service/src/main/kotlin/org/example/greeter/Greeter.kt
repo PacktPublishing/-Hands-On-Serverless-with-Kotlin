4dc0568a009e9f98ea1083bc5df3dac12f71006c
ameyawebonise <ameya.kulkarni@weboniselab.com>
2018-09-20 17:43:17 +0530

    Changes

---
+++ chapter7/secure-greeter-service/src/main/kotlin/org/example/greeter/Greeter.kt
@@ -0,0 +1,75 @@
+package org.example.greeter
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.LambdaLogger
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import org.jooq.DSLContext
+import org.jooq.Record
+import org.jooq.Result
+import org.jooq.SQLDialect
+import org.jooq.impl.DSL
+
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.Timestamp
+import java.util.Date
+
+import com.amazonaws.services.kms.AWSKMS
+import com.amazonaws.services.kms.AWSKMSClientBuilder
+import com.amazonaws.services.kms.model.DecryptRequest
+import com.amazonaws.util.Base64
+
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
+
+class Greeter : RequestHandler<Map<String, Any>, String> {
+
+
+    override fun handleRequest(input:Map<String, Any>, context: Context): String {
+        val logger = context.logger
+
+        input.forEach { (key, value) -> println("$key = $value") }
+        val userName = decryptKey("databaseUsername")
+        val password = decryptKey("databasePassword")
+        val databaseUrl = decryptKey("databaseUrl")
+        val databasePort = decryptKey("databasePort")
+        val databaseName = decryptKey("databaseName")
+
+
+        val sb = StringBuilder()
+        sb.append("jdbc:postgresql://")
+        sb.append(databaseUrl)
+        sb.append(":")
+        sb.append(databasePort)
+        sb.append("/")
+        sb.append(databaseName)
+
+        val url = sb.toString()
+
+        try {
+            val conn = DriverManager.getConnection(url, userName, password)
+            val dslContext = DSL.using(conn, SQLDialect.POSTGRES)
+            val result = dslContext.fetchOne("SELECT * FROM now()")
+            val ts = result.get("now") as java.sql.Timestamp
+            return "Hello, " + "Ameya" + " on " + ts.toString() + " as per the time on database server"
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return "Hello, " + "ameya" + " on " + Date().toString() + " as per the default time on lambda"
+    }
+
+    private fun decryptKey(environmentKeyName: String): String {
+
+        val encryptedKey = Base64.decode(System.getenv(environmentKeyName))
+
+        val client = AWSKMSClientBuilder.defaultClient()
+
+        val request = DecryptRequest()
+                .withCiphertextBlob(ByteBuffer.wrap(encryptedKey))
+
+        val plainTextKey = client.decrypt(request).plaintext
+        return String(plainTextKey.array(), Charset.forName("UTF-8"))
+    }
+}
